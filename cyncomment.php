76766f1ca412dd769fb745973d8e20c18379f7aa
Kazashim <kazashimkuzasuwat@gmail.com>
2019-06-08 13:15:37 +0200

    Initial commit

---
+++ cyncomment.php
@@ -0,0 +1,19 @@
+<?php
+
+$your_input = "This is an awesome Stuff";
+
+echo "<h3>Cynjine Commenting</h3>";
+
+echo $user_input;
+
+?>
+
+<?php
+
+$your_input = "<script>alert('this is not fun!');</script>";
+
+echo "<h4>My Commenting System</h4>";
+
+echo $your_input;
+
+?>
